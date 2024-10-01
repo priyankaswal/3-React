a60e646d3883cb27f7ab08901b17fbf740aca2a2
Priyank Aswal <aswalpriyank0@gmail.com>
2024-10-01 11:31:26 +0530

    updating 3-React

--- 10-css-modules/src/main.jsx
+++ 10-css-modules/src/main.jsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <h1 className='heading'>This is our app.</h1>
+    <h1 className="heading">This is our app 1.</h1>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
